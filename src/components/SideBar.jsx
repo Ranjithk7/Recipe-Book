6cf7c94fcf9d00bbd686f7166548cbbe49998276
ranjith <ranjith@example.com>
2024-12-26 21:02:44 +0530

    modified

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,7 +4,7 @@ import Category from "./Categories";
 
 const SideBar = ({toggle}) => {
     return (
-        <div className="w-25 position-absolute end-0 top-0 bg-dark h-100 overflow-auto" onClick={() => toggle(false)}>
+        <div className="side-bar position-fixed end-0 top-0 bg-dark h-100 overflow-auto" onClick={() => toggle(false)}>
             <li onClick={() => toggle(false)} className="bi bi-x fs-1 d-flex justify-content-end m-2"></li>
             <Country />
             <Category />
